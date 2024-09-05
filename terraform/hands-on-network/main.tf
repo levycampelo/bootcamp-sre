0e7e3341e6661166227e10bb642e7e2ff14c1933
Levy Campelo <levycampelo@gmail.com>
2024-09-05 11:03:05 -0300

    tarefa 01

--- terraform/hands-on-network/main.tf
+++ terraform/hands-on-network/main.tf
@@ -1,4 +1,4 @@
-# bucket s3
+# criar um bucket s3
 resource "aws_s3_bucket" "bootcamp-sre" {
   bucket = "bootcamp-sre" 
  
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "bootcamp-sre" {
   }
 }
 
-# vpc 
+# criar a nova vpc
 resource "aws_vpc" "bootcamp-sre" {
   cidr_block = "192.168.250.0/24"
 
@@ -17,18 +17,18 @@ resource "aws_vpc" "bootcamp-sre" {
   }
 }
 
-# subnet-public 
+# criar uma subnet-public 
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.bootcamp-sre.id
   cidr_block        = "192.168.250.0/27" 
   availability_zone = "us-east-1a"
-
+    
   tags = {
     Name = "public-subnet"
   }
 }
 
-# subnet-privada
+# criar uma subnet-privada
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.bootcamp-sre.id
   cidr_block        = "192.168.250.32/27" 
@@ -39,7 +39,7 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
-# subnet-database
+# criar uma subnet-database
 resource "aws_subnet" "database_subnet" {
   vpc_id            = aws_vpc.bootcamp-sre.id
   cidr_block        = "192.168.250.64/27"  
@@ -49,7 +49,7 @@ resource "aws_subnet" "database_subnet" {
     Name = "database-subnet"
   }
 }
-# Internet Gateway 
+# criar o internet gateway
 resource "aws_internet_gateway" "my_igw" {
   vpc_id = aws_vpc.bootcamp-sre.id
 
@@ -58,7 +58,7 @@ resource "aws_internet_gateway" "my_igw" {
   }
 }
 
-# routing-table subrede publica
+# routing-table subnet-publica
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.bootcamp-sre.id
 
@@ -72,13 +72,13 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-# associar routing-table subrede publica
+# associar routing-table subnet-publica
 resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
-# NAT Gateway subrede publica
+# criar o nat gateway subnet-publica
 resource "aws_nat_gateway" "my_nat" {
   allocation_id = aws_eip.my_eip.id
   subnet_id     = aws_subnet.public_subnet.id
@@ -88,12 +88,12 @@ resource "aws_nat_gateway" "my_nat" {
   }
 }
 
-# Elastic IP NAT Gateway
+# associar ip elastico
 resource "aws_eip" "my_eip" {
   domain = "vpc"
 }
 
-# routing-table subrede privada
+# routing-table subnet-privada
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.bootcamp-sre.id
 
@@ -107,13 +107,13 @@ resource "aws_route_table" "private_route_table" {
   }
 }
 
-# associar routing-table subrede privada
+# associar routing-table subnet-privada
 resource "aws_route_table_association" "private_subnet_association" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_route_table.id
 }
 
-# Criar uma Tabela de Rotas para a sub-rede de banco de dados
+# routing-table subnet-database
 resource "aws_route_table" "database_route_table" {
   vpc_id = aws_vpc.bootcamp-sre.id
 
@@ -122,13 +122,13 @@ resource "aws_route_table" "database_route_table" {
   }
 }
 
-# Associar a Tabela de Rotas à sub-rede de banco de dados
+#  associar routing-table subnet-database
 resource "aws_route_table_association" "database_subnet_association" {
   subnet_id      = aws_subnet.database_subnet.id
   route_table_id = aws_route_table.database_route_table.id
 }
 
-# EC2
+# criar um ec2 teste com ami amazonlinux com ssm
 resource "aws_instance" "back-end" {
   ami           = "ami-066784287e358dad1" 
   instance_type = "t2.micro" 
@@ -140,7 +140,7 @@ resource "aws_instance" "back-end" {
   iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
 }
 
-# Criar um IAM Role e uma política para o SSM
+# criar uma iam role para acesso ssm
 resource "aws_iam_role" "ssm_role" {
   name = "SSMRole"
 
@@ -158,16 +158,135 @@ resource "aws_iam_role" "ssm_role" {
   })
 }
 
-# Anexar a política gerenciada do SSM ao IAM Role
+# associar a iam role ssm
 resource "aws_iam_role_policy_attachment" "ssm_role_policy" {
   role       = aws_iam_role.ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-# Criar um IAM Instance Profile para associar ao EC2
+# criar uma iam instance para associar a ec2
 resource "aws_iam_instance_profile" "ssm_profile" {
   name = "SSMInstanceProfile"
   role = aws_iam_role.ssm_role.name
 }
 
+#### Teste do ASG
+# criar o sg para o loadbalance
+resource "aws_security_group" "lb_sg" {
+  name   = "lb_sg"
+  vpc_id = aws_vpc.bootcamp-sre.id 
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lb-sg"
+  }
+}
+
+# criar o tg para o NLB
+resource "aws_lb_target_group" "target_group" {
+  name     = "bootcamp-sre-tg"
+  port     = 80
+  protocol = "TCP"
+  vpc_id   = aws_vpc.bootcamp-sre.id
+
+  health_check {
+    protocol = "TCP"
+    port     = "traffic-port"
+  }
+
+  tags = {
+    Name = "bootcamp-sre-tg"
+  }
+}
+
+# criar o nlb
+resource "aws_lb" "nlb" {
+  name               = "bootcamp-sre-nlb"
+  internal           = false
+  load_balancer_type = "network"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [aws_subnet.public_subnet.id]
+
+  tags = {
+    Name = "bootcamp-sre-nlb"
+  }
+}
+
+# adicionar o listener
+resource "aws_lb_listener" "nlb_listener" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port              = 80
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
+# launch ec2 modelo
+resource "aws_launch_template" "ec2_template" {
+  name_prefix   = "bootcamp-sre-template"
+  image_id      = "ami-066784287e358dad1" # Amazon Linux 2
+  instance_type = "t2.micro"
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ssm_profile.name
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "bootcamp-sre-ec2"
+    }
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.lb_sg.id]
+    subnet_id                   = aws_subnet.public_subnet.id
+  }
+}
+
+# asg
+resource "aws_autoscaling_group" "asg" {
+  desired_capacity     = 1
+  max_size             = 2
+  min_size             = 1
+  vpc_zone_identifier  = [aws_subnet.public_subnet.id]
+  target_group_arns    = [aws_lb_target_group.target_group.arn]
+  launch_template {
+    id      = aws_launch_template.ec2_template.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "bootcamp-sre-ec2"
+    propagate_at_launch = true
+  }
+
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+
+  force_delete = true
+}
+
+# associar nlb para asg
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  lb_target_group_arn   = aws_lb_target_group.target_group.arn
+}
 
